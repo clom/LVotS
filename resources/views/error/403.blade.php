f637345c1113cc6c5c5bcae54a7e5e1dea015063
nomiyama_kento <nomiyama@clom-networks.com>
2017-09-19 22:45:55 +0900

    add Error Page.

---
+++ resources/views/error/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">403</div>
+                        <h3>Forbidden</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">404</div>
+                    <h3>Not Found</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
