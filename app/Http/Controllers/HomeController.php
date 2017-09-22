29f776d5654318cbc554f7d12c51758a3d464cca
nomiyama_kento <nomiyama@clom-networks.com>
2017-09-22 22:53:55 +0900

    fix Admin Checker.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,13 +47,13 @@ class HomeController extends Controller
         $code = env('ADM_CODE');
         $input_code = $req->input('k') != null ? $req->input('k') : null;
 
-        if(isset($input_code))
+        if(!isset($input_code))
             return response()->json(['msg' => 'No input Code.'],404);
 
-        if($code == $input_code)
+        if($code != $input_code)
             return response()->json(['msg' => 'No match Code.'],401);
 
-        $adm_data = DB::table('users')->where('adm', 1)->get();
+        $adm_data = DB::table('users')->where('adm', 1)->first();
 
         if(!empty($adm_data))
             return response()->json(['msg' => 'Already Admin.'],401);
